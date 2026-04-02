3cfefbeba60cad7e00448e1cb28ba57a899bbdaa
JAlavindu <lavi156perera@gmail.com>
2026-04-02 15:52:37 +0530

    getting questions

--- microservices/question-service/src/main/java/com/lavindu/question_service/service/QuestionService.java
+++ microservices/question-service/src/main/java/com/lavindu/question_service/service/QuestionService.java
@@ -28,9 +28,28 @@ public class QuestionService {
 
     public ResponseEntity<List<Integer>> getQuestionsForQuiz(String categoryName, Integer numQuestions) {
         List<Integer> questions = questionDao.findRandomQuestionsByCategory(categoryName, numQuestions);
-        
-
         return new ResponseEntity<>(questions, HttpStatus.OK);
     }
-    
+
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionIds) {
+        List<QuestionWrapper> wrappers = new ArrayList<>();
+        List<Question> questions = new ArrayList<>();
+
+        for(Integer id : questionIds){
+            questions.add(questionDao.findById(id).get());
+        }
+
+        for(Question question : questions){
+            QuestionWrapper qw = new QuestionWrapper();
+            qw.setId(question.getId());
+            qw.setQuestionTitle(question.getQuestionTitle());
+            qw.setOption1(question.getOption1());
+            qw.setOption2(question.getOption2());
+            qw.setOption3(question.getOption3());
+            qw.setOption4(question.getOption4());
+            wrappers.add(qw);
+        }
+        return new ResponseEntity<>(wrappers, HttpStatus.OK);
+    }
+
 }
