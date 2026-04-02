66a3423924009874f746eba2dd929818dd2a27b6
JAlavindu <lavi156perera@gmail.com>
2026-04-02 12:49:16 +0530

    started quiz-service

---
+++ microservices/question-service/src/test/java/com/lavindu/question_service/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavindu.question_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
